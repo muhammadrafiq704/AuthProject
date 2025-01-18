2d977408a34d66a2821eb3cc72dab7d4bd141b83
Muhammad Rafiq <sameerbalti704@gmail.com>
2025-01-18 15:57:29 +0500

    logo image

--- AuthProject/src/components/Logo/index.tsx
+++ AuthProject/src/components/Logo/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { LogoSection } from "./styled";
 import QuiltedImageList from "./Gallery";
+import LogoImage from "/assets/images/logo.png"
 
 const Logo: React.FC = () => {
   return (
     <>
       <LogoSection>
         <div>
-          <img src="/assets/images/logo.png" alt="logo here..." />
+          <img src={LogoImage} alt="logo here..." />
           <h3>KnobExpress</h3>
         </div>
         <QuiltedImageList />
