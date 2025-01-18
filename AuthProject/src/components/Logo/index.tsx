6832c66f315110d0a9de45a05f0a369c8d07116a
Muhammad Rafiq <sameerbalti704@gmail.com>
2025-01-18 15:49:09 +0500

    logo and lines fixed

--- AuthProject/src/components/Logo/index.tsx
+++ AuthProject/src/components/Logo/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { LogoSection } from "./styled";
 import QuiltedImageList from "./Gallery";
+
 const Logo: React.FC = () => {
   return (
     <>
       <LogoSection>
         <div>
-          <img src="src/assets/images/logo.png" alt="logo here..." />
+          <img src="/assets/images/logo.png" alt="logo here..." />
           <h3>KnobExpress</h3>
         </div>
         <QuiltedImageList />
