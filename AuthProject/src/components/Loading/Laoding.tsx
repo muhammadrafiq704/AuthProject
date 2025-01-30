ee00870837f4a3e3978193fc1fd922cf56f4328d
Muhammad Rafiq <sameerbalti704@gmail.com>
2025-01-31 01:39:53 +0500

    mui field loading state local and global

---
+++ AuthProject/src/components/Loading/Laoding.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styled.css";
+
+const Laoding: React.FC = () => {
+  return (
+    <section className="dots-container">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </section>
+  );
+};
+
+export default Laoding;
